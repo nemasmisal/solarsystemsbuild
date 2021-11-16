d3ae13729a7086a272e412e3e2f3975816a0d63c
nikolay <nikolay.topalov@gmail.com>
2021-11-16 21:20:35 +0200

    basic structure

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const components = [NavbarComponent, FooterComponent]
+
+@NgModule({
+  declarations: [
+    ...components
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
