0af422a3c04a37e66b2e066ddda015f7598f5439
nikolay <nikolay.topalov@gmail.com>
2021-11-18 22:04:06 +0200

    side nav + hamburger for mobile added, social icons svg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import {DashboardModule} from './dashboard/dashboard.module';
-
+import { DashboardModule } from './dashboard/dashboard.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,9 +14,10 @@ import {DashboardModule} from './dashboard/dashboard.module';
     AppRoutingModule,
     CoreModule,
     SharedModule,
-    DashboardModule,   
+    DashboardModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
