1c8bba43a99401442d1acb3ae2720fd92b424c26
dena8 <Perindopril88@>
2021-11-20 13:13:13 +0200

    create projects

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-import {RouterModule} from '@angular/router'
-
-
+import { RouterModule } from '@angular/router';
+import { CoreModule } from '../core/core.module';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
+  declarations: [HomeComponent, ProjectsComponent],
   imports: [
     CommonModule,
+    CoreModule,
     RouterModule.forRoot([
-      {path:'home',component:HomeComponent}
-    ])
-  ]
+      { path: 'home', component: HomeComponent },
+      { path: 'projects', component: ProjectsComponent },
+    ]),
+  ],
 })
-export class DashboardModule { }
+export class DashboardModule {}
