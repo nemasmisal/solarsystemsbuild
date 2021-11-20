038149b2fe1d40a14827cb9e916e97642f1a3499
dena8 <Perindopril88@>
2021-11-20 12:37:48 +0200

    Styling navbar, which is main layout navbar for the application.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NavbarHomeComponent } from './components/navbar-home/navbar-home.component';
 
-const components = [NavbarComponent, FooterComponent]
+
+const components = [NavbarComponent, FooterComponent, NavbarHomeComponent];
 
 @NgModule({
   declarations: [
-    ...components
+    ...components,    
   ],
-  imports: [
-    CommonModule,   
+  imports: [   
+    CommonModule,
+        
   ],
-  exports:[NavbarComponent,FooterComponent]
+  exports:[NavbarComponent,FooterComponent,NavbarHomeComponent]
 })
 export class CoreModule { }
